4fab10dcb580cf6cd070014f863899b285c9a98f
TahaZ4 <abdullahtaha5818@gmail.com>
2026-04-19 21:28:59 +0200

    Fix formatting issues in encryption.cpp

--- Secure_Server_M1/encryption.cpp
+++ Secure_Server_M1/encryption.cpp
@@ -1,15 +1,17 @@
 #include "encryption.h"
 
+// Client-side: Shift ASCII up by 3
 std::string encrypt(std::string data) {
     for (int i = 0; i < data.length(); i++) {
-        data[i] = data[i] + 3; // Shift ASCII up by 3
+        data[i] = data[i] + 3;
     }
     return data;
 }
 
+// Server-side: Shift ASCII down by 3
 std::string decrypt(std::string data) {
     for (int i = 0; i < data.length(); i++) {
-        data[i] = data[i] - 3; // Shift ASCII down by 3
+        data[i] = data[i] - 3;
     }
     return data;
 }
